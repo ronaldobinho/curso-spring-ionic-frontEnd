511140c5bafcff6d4734822a4bd900384c2f2ea3
ronaldobinho@gmail.com <ronaldobinho@gmail.com>
2018-05-24 10:17:33 -0300

    Botao flutuante acesso ao carrinho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Profile', component: "ProfilePage" },      
-      { title: 'Categorias', component: "CategoriasPage" },
+      { title: 'Categorias', component: "CategoriasPage" },      
+      { title: 'Carrinho', component: "CartPage" },
       { title: 'Logout', component: "" }
     ];
 

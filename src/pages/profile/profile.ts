5a36276c1aac9dedfe7946a768886028bff148e0
ronaldobinho@gmail.com <ronaldobinho@gmail.com>
2018-05-25 12:23:47 -0300

    Usando o cordova para capturar foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,6 +4,7 @@ import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
 import { ClienteDTO } from '../../models/clinte.dto';
 import { StorageService } from '../../services/storage.services';
+import { CameraOptions, Camera } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -13,12 +14,15 @@ import { StorageService } from '../../services/storage.services';
 export class ProfilePage {
 
   cliente: ClienteDTO;
+  picture : string;
+  cameraON : boolean = false;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+  public camera : Camera ) {
   }
 
   ionViewDidLoad() {
@@ -46,5 +50,25 @@ export class ProfilePage {
       this.cliente.imageURL = `${API_CONFIG.buckectBaseUrl}/cp${this.cliente.id}.jpg`;
     },
     error => {});
+  }
+
+  getCameraPicture(){
+     this.cameraON = true;
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     this.picture = 'data:image/png;base64,' + imageData;
+     
+     this.cameraON = false;
+    }, (err) => {
+     // Handle error
+    });
   }
 }

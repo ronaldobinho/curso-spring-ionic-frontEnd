a64e4a79603aa37cec4634ea8713e77aab9e8020
ronaldobinho@gmail.com <ronaldobinho@gmail.com>
2018-05-25 08:27:16 -0300

    Armazenando os dados do pedido

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { StorageService } from '../../services/storage.services';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartService } from '../../services/domain/cart.services';
 
 /**
  * Generated class for the PickAddressPage page.
@@ -18,44 +22,49 @@ export class PickAddressPage {
 
   items : EnderecoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  pedido : PedidoDTO;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public storage : StorageService,
+    public clienteService : ClienteService,
+    public cartService : CartService
+  ) {
   }
   
   ionViewDidLoad() {
-    this.items = [
-      {
-        id: "1",
-        logradouro: "Rua Quinze de Novembro",
-        numero: "300",
-        complemento: "Apto 200",
-        bairro: "Santa Mônica",
-        cep: "48293822",
-        cidade: {
-          id: "1",
-          nome: "Uberlândia",
-          estado: {
-            id: "1",
-            nome: "Minas Gerais"
+    let localUser = this.storage.getLocalUser();
+    if (localUser && localUser.email) {
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.items = response['enderecos'];
+
+          let cart = this.cartService.getCart();
+
+          this.pedido = {
+            cliente: { id : response['id']},
+            enderecoDeEntrega : null,
+            pagamento : null, 
+            itens : cart.itens.map(x => {return {quantidade: x.quantidade, produto: {id: x.produto.id} }})
+            
           }
-        }
-      },
-      {
-        id: "2",
-        logradouro: "Rua Alexandre Toledo da Silva",
-        numero: "405",
-        complemento: null,
-        bairro: "Centro",
-        cep: "88933822",
-        cidade: {
-          id: "3",
-          nome: "São Paulo",
-          estado: {
-            id: "2",
-            nome: "São Paulo"
+        },
+        error => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
           }
-        }
-      }
-    ];
+        });
+    }
+    else {
+      this.navCtrl.setRoot('HomePage');
+    }
+  }
+
+  nextPage(item : EnderecoDTO) {
+    this.pedido.enderecoDeEntrega = {id : item.id};
+    console.log(this.pedido);
+    
   }
 
 }
